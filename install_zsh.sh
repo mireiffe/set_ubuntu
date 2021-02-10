911402ec57bfabdc8873a4053a5771db10cf5841
wisrkiyr <wisrkiyr@gmail.com>
2021-02-10 16:17:28 +0900

    hello

---
+++ install_zsh.sh
@@ -0,0 +1,6 @@
+sudo apt-get zsh -y &&\
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&\
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&\
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&\
+sed -i '/^ZSH_THEME/a\ZSH_THEME="kafeitu"' ~/.zshrc &&\
+sed -i 's/^plugins.*/plugins=(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n)/' ~/.zshrc
