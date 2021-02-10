91676e2a2299b03b2eb988d990034e97ecde198c
mireiffe <mireiffe@kaist.ac.kr>
2021-02-10 17:47:47 +0900

    mdfy

--- src/install_chrome.sh
+++ src/install_chrome.sh
@@ -1,4 +1,2 @@
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&\
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&\
-sudo apt-get update &&\
-sudo apt-get install google-chrome-stable -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&\
+apt install ./google-chrome-stable_current_amd64.deb
--- src/install_zsh.sh
+++ src/install_zsh.sh
@@ -1,5 +1,6 @@
-sudo apt-get install zsh -y &&\
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt-get install zsh -y &&\
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh | no &&\
+chsh -s /usr/bin/zsh &&\
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&\
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&\
 sed -i '/^ZSH_THEME/a\ZSH_THEME="kafeitu"' ~/.zshrc &&\
