2970e4327120f81603b76396ad8eae6ec2e4e4c3
mireiffe <mireiffe@kaist.ac.kr>
2021-02-10 08:28:34 +0000

    mdfy

---
+++ src/setup_all.sh
@@ -0,0 +1,10 @@
+_HOME=`pwd`
+
+sh ${_HOME}/src/install_basics.sh
+sh ${_HOME}/src/install_zsh.sh
+sh ${_HOME}/src/install_ohmyzsh_plugins.sh
+sh ${_HOME}/src/install_vscode.sh
+sh ${_HOME}/src/install_miniconda.sh
+sh ${_HOME}/src/install_docker.sh
+sh ${_HOME}/src/install_chrome.sh
+sh ${_HOME}/src/install_tilix.sh
