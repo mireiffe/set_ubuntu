da99cd11d4f7af46c8c36964586996b15cde0d53
mireiffe <mireiffe@kaist.ac.kr>
2021-02-15 10:54:03 +0900

    mdfy

--- src/install_zsh.sh
+++ src/install_zsh.sh
@@ -1,6 +1,5 @@
 sudo apt-get install zsh -y &&\
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /usr/bin/zsh &&\
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&\
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&\
 sed -i '/^ZSH_THEME/a\ZSH_THEME="kafeitu"' ~/.zshrc &&\
