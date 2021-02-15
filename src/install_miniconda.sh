d655c07d933f9bae41fc3805d859f3f0cf9985ee
mireiffe <mireiffe@kaist.ac.kr>
2021-02-15 10:21:10 +0900

    mdfy

--- src/install_miniconda.sh
+++ src/install_miniconda.sh
@@ -1,2 +1,2 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh &&\
-sudo apt-get install ./Miniconda3-latest-Linux-x86.sh
+apt-get install ./Miniconda3-latest-Linux-x86.sh -y
