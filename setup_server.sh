0e6f05e245291b600fe2c0a25f30296d872cc7cd
mireiffe <mireiffe@kaist.ac.kr>
2021-02-15 11:31:21 +0900

    mdfy

---
+++ setup_server.sh
@@ -0,0 +1,6 @@
+_HOME=`pwd`
+
+sh ${_HOME}/src/install_basics.sh
+sh ${_HOME}/src/install_zsh.sh
+sh ${_HOME}/src/install_miniconda.sh
+sh ${_HOME}/src/install_docker.sh
--- src/install_miniconda.sh
+++ src/install_miniconda.sh
@@ -1,2 +1,2 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh &&\
-sudo apt-get install ./Miniconda3-latest-Linux-x86.sh -y
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh &&\
+sudo apt-get install ./Miniconda3-latest-Linux-x86_64.sh -y
