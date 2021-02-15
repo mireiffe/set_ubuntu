a4688edb4f0e19591547d86a2e68f60ae55469c4
mireiffe <mireiffe@kaist.ac.kr>
2021-02-15 10:34:36 +0900

    mdfy

--- setup_all.sh
+++ setup_all.sh
@@ -2,7 +2,6 @@ _HOME=`pwd`
 
 sh ${_HOME}/src/install_basics.sh
 sh ${_HOME}/src/install_zsh.sh
-chsh -s /usr/bin/zsh
 sh ${_HOME}/src/install_vscode.sh
 sh ${_HOME}/src/install_miniconda.sh
 sh ${_HOME}/src/install_docker.sh
--- src/install_zsh.sh
+++ src/install_zsh.sh
@@ -1,5 +1,5 @@
 sudo apt-get install zsh -y &&\
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh | no &&\
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh &&\
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&\
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&\
