7610ee4701658667927bf8622e8cc4f6c61459af
mireiffe <mireiffe@kaist.ac.kr>
2021-03-02 15:43:43 +0900

    ndfy

--- setup_server.sh
+++ setup_server.sh
@@ -3,4 +3,4 @@ _HOME=`pwd`
 sh ${_HOME}/src/install_basics.sh
 sh ${_HOME}/src/install_zsh.sh
 sh ${_HOME}/src/install_miniconda.sh
-sh ${_HOME}/src/install_docker.sh
+#sh ${_HOME}/src/install_docker.sh
